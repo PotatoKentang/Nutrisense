6793615ff831ec3bea683fea3943e3e80b0b18d2
jeffjuann <jeffjuann@gmail.com>
2023-04-27 19:21:18 +0700

    fix gsap intro

--- js/introduction.js
+++ js/introduction.js
@@ -2,11 +2,9 @@ var vid = document.getElementById("intro-vid");
 vid.playbackRate = 2;
 
 const tl = gsap.timeline();
-tl.to(".video-wrap",{
-    duration: 3
-});
 tl.to(".txt",{
     opacity: 1,
-    duration: 1,
+    delay: 15,
+    duration: 2,
     stagger: .25
 })
